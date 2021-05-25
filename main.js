19ac7ed51bec8bf9d111cba21551ba245d55b195
Younous <Younissadam2020@gmail.com>
2021-05-25 16:35:26 -0400

    Enum and others types

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
-function doSomething() {
-    for (var i = 0; i < 5; i++) {
-        console.log(i);
-    }
-    console.log("Finally " + i);
-}
-doSomething();
+var count = 5;
+count = "a";
+// any variable, using type annotations
+var a; // we set that type of variable to number
+a = 1;
+// a = true;
+// a = "a";
+// our different tupe
+var b;
+var c;
+var d;
+var e = [1, 2, 3];
+var f = [1, true, "a"];
+//Our type ENUM, working with related constants
+var ColorRed = 0;
+var ColorGreen = 2;
+var ColorBlue = 2;
+// our we can use ENUM FOR MORE CLEANER
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 1] = "Red";
+    Color[Color["Green"] = 2] = "Green";
+    Color[Color["Blue"] = 3] = "Blue";
+})(Color || (Color = {}));
+// we done need to say = 1 or =2 is automatically assigned
+var backgroundColor = Color.Red;
--- main.ts
+++ main.ts
@@ -1,9 +1,30 @@
-function doSomething() {
-    for(var i =0; i<5; i++){
-        console.log(i)
-    }
-    console.log(`Finally ${i}`)
+let count = 5;
+count = "a";
+
+// any variable, using type annotations
+let a: number; // we set that type of variable to number
+a = 1;
+// a = true;
+// a = "a";
+
+// our different tupe
+let b: boolean;
+let c: string;
+let d: any;
+let e: number[] = [1, 2, 3];
+let f: any[] = [1, true, "a"];
+
+//Our type ENUM, working with related constants
+const ColorRed = 0;
+const ColorGreen = 2;
+const ColorBlue = 2;
+
+// our we can use ENUM FOR MORE CLEANER
+enum Color {
+  Red = 1,
+  Green = 2,
+  Blue = 3,
 }
+// we done need to say = 1 or =2 is automatically assigned
 
-
-doSomething()
+let backgroundColor = Color.Red;
