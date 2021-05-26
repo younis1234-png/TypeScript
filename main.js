7c998078966452c92e877b7c8b04da32837ea3d9
Younous <Younissadam2020@gmail.com>
2021-05-25 21:52:56 -0400

    constructor and make it the parameters optional;

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
-// Objects
+//Constructor
+// this Point is an class
 var Point = /** @class */ (function () {
-    function Point() {
+    function Point(x, y) {
+        this.x = x;
+        this.y = y;
     }
     Point.prototype.draw = function () {
         console.log("X: " + this.x + "  Y: " + this.y + " ");
     };
-    Point.prototype.getDistance = function (another) {
-        //..
-    };
     return Point;
 }());
-var point = new Point();
-point.x = 1;
-point.y = 2;
+//this (point) is an object
+var point = new Point(1, 2);
 point.draw();
-// When defining an object of a custom type,
-// we need to explicitly allocate memory to it (new Point())
+//
--- main.ts
+++ main.ts
@@ -1,25 +1,22 @@
-// Objects
+//Constructor
 
 // this Point is an class
 class Point {
   x: number;
   y: number;
+  // adding ? to make it optional 
+  constructor(x?: number, y?: number) {
+    this.x = x;
+    this.y = y;
+  }
 
   draw() {
     console.log(`X: ${this.x}  Y: ${this.y} `);
   }
-
-  getDistance(another: Point) {
-    //..
-  }
 }
 
 //this (point) is an object
-let point = new Point();
-point.x = 1;
-point.y = 2;
+let point = new Point(1, 2);
 point.draw();
 
-// When defining an object of a custom type,
-// we need to explicitly allocate memory to it (new Point())
-// output : X: 1  Y: 2
+//
