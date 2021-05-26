bbb1b9df525d9ca87e6ec5e790169e7ef9df5ef4
Younous <Younissadam2020@gmail.com>
2021-05-25 22:00:35 -0400

    Acess Modifier : Private Public or Protected

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-//Constructor
+//Access Modifier
 // this Point is an class
 var Point = /** @class */ (function () {
+    // adding ? to make it optional
     function Point(x, y) {
         this.x = x;
         this.y = y;
     }
+    // method
     Point.prototype.draw = function () {
         console.log("X: " + this.x + "  Y: " + this.y + " ");
     };
@@ -12,5 +14,10 @@ var Point = /** @class */ (function () {
 }());
 //this (point) is an object
 var point = new Point(1, 2);
+// We can change the x and y values
+// by doing point.x =3 or....
+// but how can we avoid that??
+// Access Modifier: we can apply this to a MEMBER OF A CLASS TO CONTROL IT ACCESSIBILITY
+// Access modifier is either = default is Public , Private, Protected
+// Adding private to x you can see it when you do (point.x)
 point.draw();
-//
--- main.ts
+++ main.ts
@@ -1,15 +1,16 @@
-//Constructor
+//Access Modifier
 
 // this Point is an class
 class Point {
-  x: number;
-  y: number;
-  // adding ? to make it optional 
+  private x: number;
+  private y: number;
+  // adding ? to make it optional
   constructor(x?: number, y?: number) {
     this.x = x;
     this.y = y;
   }
 
+  // method
   draw() {
     console.log(`X: ${this.x}  Y: ${this.y} `);
   }
@@ -17,6 +18,10 @@ class Point {
 
 //this (point) is an object
 let point = new Point(1, 2);
+// We can change the x and y values
+// by doing point.x =3 or....
+// but how can we avoid that??
+// Access Modifier: we can apply this to a MEMBER OF A CLASS TO CONTROL IT ACCESSIBILITY
+// Access modifier is either = default is Public , Private, Protected
+// Adding private to x you can see it when you do (point.x)
 point.draw();
-
-//
